fbb990e87f7a235fbf31a734f178745c271e0c72
sdd <dd.sh@163.com>
2023-06-03 08:02:49 +0800

    change jpillora to my repo

--- example/main.go
+++ example/main.go
@@ -5,8 +5,8 @@ import (
 	"net/http"
 	"time"
 
-	"github.com/jpillora/overseer"
-	"github.com/jpillora/overseer/fetcher"
+	"github.com/sunbird1015/overseer"
+	"github.com/sunbird1015/overseer/fetcher"
 )
 
 //see example.sh for the use-case
